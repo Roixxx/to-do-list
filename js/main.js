64e5a01ecbc523d7b3a92340657c4e3cafa2651d
Roi <vosroix@gmail.com>
2020-07-17 17:48:07 +0500

    added new functions: generateTask, addTask, clearInputText

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log('s')
+const addBtn = document.querySelector(".todo__input-btn");
+const inputText = document.querySelector(".todo__input-text");
+const tasksListField = document.querySelector(".todo__list");
+
+
+
+let tasksList = [];
+
+
+function generateTask() { 
+    
+    let task = {
+        todo: inputText.value,
+        date: 1703,
+        checked: false,
+    }
+
+    tasksList.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+    addTask();
+
+}
+
+function addTask() {
+
+    let tasksArr = JSON.parse(localStorage.getItem('tasks'));
+    let task = tasksArr[tasksArr.length - 1];
+
+    tasksListField.innerHTML += `<li> ${task.todo} </li>`; 
+    
+
+    clearInputText();
+}
+
+function clearInputText() {
+    inputText.value = '';
+}
+
+
+
+addBtn.onclick = function () {
+    if (!inputText.value) return;
+    generateTask();
+}
+
+
+inputText.onkeypress = function (e) {
+    e.key == 'Enter' ? generateTask() : null; 
+}
