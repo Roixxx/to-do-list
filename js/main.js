bcc007e888daa485b22d5c2a820b27f5548148c8
Roi <vosroix@gmail.com>
2020-07-27 16:34:25 +0500

    added a checkbox

--- js/main.js
+++ js/main.js
@@ -45,8 +45,9 @@ function generateTask() {
 function generateHtmlTask(task) {
     let taskHtml = 
     `<li class="todo__item" data-taskId="${task.taskId}"> 
+        <input type="checkbox" onclick="completeTask(this)">
         <p class="todo__task">${task.todo}</p> 
-        <div>
+        <div class="todo__btns-holder">
             <input class="todo__delete" onclick="delTask(this)" type="button" value="delete">
             <input class="todo__edit" onclick="editTask(this)" type="button" value="edit">
         </div>
@@ -61,12 +62,17 @@ function addTask(taskHtml) {
     clearInputText();
 }
 
+function completeTask(task) {
+    return;
+}
+
 function delTask(target) {
     target = target.parentNode.parentNode;
     let targetId = target.dataset.taskid;
 
-    tasksList = tasksList.splice(targetId, 1);
+    tasksList.splice(targetId, 1);
     localStorage.setItem('tasks', JSON.stringify(tasksList));
+    console.log(tasksList)
 
     target.remove();
 }
